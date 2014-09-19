9dfcf8f2b036d8e444809dea83b8595609007887
cristo-rabani <cristo.rabani@gmail.com>
2014-09-19 20:37:50 +0200

    to

--- UniCollection.js
+++ UniCollection.js
@@ -1,23 +1,64 @@
+//UniCollection = function(){
+//    Meteor.Collection.apply(this, arguments);
+//    var self = this;
+//
+//    self.Document = function(doc) { return _.extend(this, doc); };
+//    self._transform = function(doc) { return new self.Document(doc); };
+//
+//    this.helpers = function(helpers) {
+//        _.each(helpers, function(helper, key) {
+//            self.Document.prototype[key] = helper;
+//        });
+//    }
+//
+//    this.helpers(_.extend({getCollection: function(){
+//        return self;
+//    }}, UniDoc));
+//
+//};
+//
+//
+//var UniCollectionPrototype = function(){ this.constructor = UniCollection; };
+//UniCollectionPrototype.prototype = Meteor.Collection.prototype;
+//UniCollection.prototype = new UniCollectionPrototype;
+
+////////////////////////////
 UniCollection = function(){
     Meteor.Collection.apply(this, arguments);
-    var self = this;
-
-    self.Document = function(doc) { return _.extend(this, doc); };
-    self._transform = function(doc) { return new self.Document(doc); };
-
-    this.helpers = function(helpers) {
-        _.each(helpers, function(helper, key) {
-            self.Document.prototype[key] = helper;
-        });
-    }
-
-    this.helpers(_.extend({getCollection: function(){
-        return self;
-    }}, UniDoc));
-
+    this._helpers = {};
+    this.setDoc(UniDoc);
 };
-
 
 var UniCollectionPrototype = function(){ this.constructor = UniCollection; };
 UniCollectionPrototype.prototype = Meteor.Collection.prototype;
 UniCollection.prototype = new UniCollectionPrototype;
+
+UniCollection.prototype.setDoc = function(doc){
+    var self = this;
+    this._docModel = doc;
+    this._transform = function(doc){
+        doc = self._docModel(doc);
+        doc.getCollection = function() { return self; };
+        _.extend(doc, this._helpers);
+        return doc;
+    };
+};
+
+UniCollection.prototype.helpers = function(helpers) {
+    var self = this;
+    _.each(helpers, function(helper, key) {
+        self._helpers[key] = helper;
+    });
+};
+
+UniBook = function(doc){
+    doc.isReaded = false;
+
+    doc.read = function(){
+        this.isReaded = true;
+    }
+
+    return doc;
+};
+
+UniBook = _.compose(UniBook, UniDoc);
--- UniDoc.js
+++ UniDoc.js
@@ -1,14 +1,25 @@
-UniDoc = {
-    update: function (modifier, options, cb) {
-        return this.getCollection().update(this._id, modifier, options, cb);
-    },
-    remove: function ( cb) {
-        return this.getCollection().remove(this._id, cb);
-    },
-    save: function(){
-    var keys = this.getCollection().simpleSchema().objectKeys();
-    var doc = _.pick(this, keys);
-        console.log(doc, this);
-        return this.update({$set: doc});
-    }
+UniDoc = function (doc) {
+    var self = this;
+    doc.update = function (modifier, options, cb) {
+        return self.update(this._id, modifier, options, cb);
+    };
+    doc.remove = function (cb) {
+        return self.remove(this._id, cb);
+    };
+    doc.save = function () {
+        var data = {};
+        var schema = self.simpleSchema();
+        _.each(doc, function(v, k){
+            if(!_.isFunction(v)){
+                var p ={};
+                p[k] =v;
+                if(schema.newContext().validateOne(p, k)){
+                    data[k] = v;
+                }
+            }
+        });
+        console.log(data);
+        return doc.update({$set: data});
+    };
+    return doc;
 };
