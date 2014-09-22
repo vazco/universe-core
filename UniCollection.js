aa35ef0f75e79dcc8c1f176ba280a0628654845c
cristo-rabani <cristo.rabani@gmail.com>
2014-09-22 13:00:18 +0200

    tak

--- UniCollection.js
+++ UniCollection.js
@@ -8,16 +8,28 @@ UniCollection = function(){
 var UniCollectionPrototype = function(){ this.constructor = UniCollection; };
 UniCollectionPrototype.prototype = Meteor.Collection.prototype;
 UniCollection.prototype = new UniCollectionPrototype;
+UniCollection.prototype._checkModel = function(docModel){
+    var docObject = docModel({});
+    if(_.isObject(docObject)){
+        for(prop in docObject){
+            if(_.has(docObject, prop) && !_.isFunction(prop)){
+                throw new Meteor.Error(500, 'DocModel must contain only functions');
+            }
+        }
+    }
+};
 
-UniCollection.prototype.setDoc = function(doc){
+UniCollection.prototype.setDoc = function(docModel){
     var self = this;
-    this._docModel = doc;
+    this._checkModel(docModel);
+
+    this._docModel = docModel;
+    _.each(self._docHelpers, function(helper, key) {
+        doc[key] = helper;
+    });
     this._transform = function(doc){
         self._docModel(doc);
         doc.getCollection = function() { return self; };
-        _.each(self._docHelpers, function(helper, key) {
-            doc[key] = helper;
-        });
         return doc;
     };
 };
