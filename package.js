35f3e7860a4e806c06e8c4474eb3cce7df6e5545
cristo-rabani <cristo.rabani@gmail.com>
2014-11-25 17:38:46 +0100

    package.js

--- package.js
+++ package.js
@@ -1,13 +1,15 @@
 Package.describe({
     summary: 'Universe CMS Core package',
-    name: 'vazco:universe-core'
+    name: 'vazco:universe-core',
+    version: "0.9.0",
+    git: "https://cristo-rabani@bitbucket.org/vazco/universe-core.git"
 });
 
 Package.on_use(function (api) {
     api.use([
-        'underscore',
-        'accounts-base',
-        'aldeed:simple-schema'
+        'underscore@1.0.1',
+        'accounts-base@1.1.2',
+        'aldeed:simple-schema@0.0.0'
     ], ['client', 'server']);
 
     api.use(['matb33:collection-hooks'], ['client', 'server'], {weak: true});
