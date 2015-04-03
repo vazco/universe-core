034b4652de4d9cdc9cb3f62270d7d40159f800b5
cristo-rabani <cristo.rabani@gmail.com>
2015-04-03 14:55:57 +0200

    ttt

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: 'Contains collection on steroids. ( access / doc helpers / user helpers ) Many awesome utilities',
     name: 'vazco:universe-core',
-    version: '1.6.5',
+    version: '1.6.6',
     git: 'https://github.com/vazco/universe-core.git'
 });
 
@@ -10,7 +10,7 @@ Package.on_use(function (api) {
     var implyPackages = [
         'vazco:universe-utilities@1.0.3',
         'vazco:universe-collection@1.0.5',
-        'vazco:universe-access@1.1.1'
+        'vazco:universe-access@1.1.2'
     ];
     api.use(implyPackages);
     api.imply(implyPackages);
